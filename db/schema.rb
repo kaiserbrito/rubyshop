90e3b0baba1facaa80177550345d6fadc745346a
kaiserbrito <victorbcguerra@gmail.com>
2017-06-28 16:21:39 -0300

    made the receipt more secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628183819) do
+ActiveRecord::Schema.define(version: 20170628191750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170628183819) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
   create_table "spree_addresses", id: :serial, force: :cascade do |t|
